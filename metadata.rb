49b1ad79fbe87d26a744da0723c05e9967ef8607
Carl Eichhorn <ceichhorn@gannett.com>
2016-08-04 13:40:55 -0400

    Initial cookbook

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
 # add any cookbook dependencies here
-# depends 'other_cookbook_name'
+depends 'gdp-base-linux', '>= 2.0.7'
 
 supports 'centos'
 
---
+++ spec/recipes/default_spec.rb
@@ -0,0 +1,60 @@
+#
+# Cookbook Name:: sauceconnect
+# Spec:: default
+#
+# Copyright (c) 2016 Gannett Co., Inc, All Rights Reserved.
+
+require 'spec_helper'
+
+
+describe 'sauceconnect::default' do
+  context 'When all attributes are default, on centos' do
+    cached(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'centos', version: '7.1.1503') do |node|
+        Chef::Config[:client_key] = "/etc/chef/client.pem"
+        node.set['sauceconnect']['server']['install_dir'] = '/opt/sauceconnect'
+        node.set['sauceconnect']['server']['version'] = '4.3.16'
+        stub_command("which sudo").and_return('/usr/bin/sudo')
+        node.set['authorization']['sudo']['groups'] = ["admin", "wheel", "test"]
+        node.set['ssh_keys'] = ''
+        node.default['ssh_keys'] = { test: ["test"] }
+      end.converge(described_recipe)
+    end
+    # included recipes
+    recipe_list = ['gdp-base-linux']
+
+    recipe_list.each do |name|
+      it 'includes the recipe ' + name do
+        expect(chef_run).to include_recipe(name)
+      end
+    end
+
+    it 'creates sauceconnect user' do
+      expect(chef_run).to create_user('sauceprx')
+    end
+  
+    directory_list = ['/opt/sauceconnect']
+    directory_list.each do |name|
+      it 'creates '+ ' directory' do
+        expect(chef_run).to create_directory(name)
+      end
+    end
+
+    it 'downloads app file' do
+      expect(chef_run).to create_remote_file('/tmp/sc-4.3.16-linux.tar.gz')
+    end
+
+    
+
+     it 'creates the sauceconnect config file' do
+      expect(chef_run).to create_template('/etc/sysconfig/sauceconnect')
+    end
+
+    it 'starts sauceconnect after it is installed' do
+      expect(chef_run).to notify('service[sauceconnect]').to(:start)
+    end
+
+
+ end
+
+end
