81d8df29fadc9b7349c65eb07cc18012645c7784
Carl Eichhorn <ceichhorn@gannett.com>
2016-08-11 09:56:29 -0400

    modified Berksfile

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -53,7 +53,7 @@ describe 'sauceconnect::default' do
     end
 
      it 'creates the sauceconnect config file' do
-      expect(chef_run).to create_template('/etc/sysconfig/sauceconnect.conf')
+      expect(chef_run).to create_template('/opt/sauceconnect/sauceconnect.conf')
     end
 
     it 'executes both actions' do
